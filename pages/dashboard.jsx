3c584234519c6523c15265163fbfc02b520095fd
Nagasai37 <23ht1a0537@gmail.com>
2026-07-13 14:54:08 +0530

    added pages with components

---
+++ pages/dashboard.jsx
@@ -0,0 +1,21 @@
+function Dashboard(){
+
+return(
+
+<div>
+
+<h1>Dashboard</h1>
+
+<h3>Total Products : 100</h3>
+
+<h3>Orders : 230</h3>
+
+<h3>Customers : 60</h3>
+
+</div>
+
+)
+
+}
+
+export default Dashboard;
